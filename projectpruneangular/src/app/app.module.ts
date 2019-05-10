fbdd36c9dd143ed8a432dbc2c3ce9086df4cd142
Andris Roling <arol@d-velop.de>
2019-05-10 10:20:59 +0200

    fixed tslint

--- projectpruneangular/src/app/app.module.ts
+++ projectpruneangular/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
