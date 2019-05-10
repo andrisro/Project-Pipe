4d9d09a6468989cd24aec60a67eab4cbb24f34bf
Andris Roling <arol@d-velop.de>
2019-05-10 13:25:35 +0200

    repaired material design & added login form

--- projectpruneangular/src/app/app.module.ts
+++ projectpruneangular/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
-import {LoginComponent} from "./components/login/login.component";
-import {MaterialModule} from "./material.module";
+import {LoginComponent} from './components/login/login.component';
+import {MaterialModule} from './material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,6 @@ import {MaterialModule} from "./material.module";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
--- projectpruneangular/src/app/components/login/login.component.ts
+++ projectpruneangular/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ApiService} from '../../common/services/api.service';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
