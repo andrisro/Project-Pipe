69bcb9c029be2ab61b6ef6e228a4773f7ae6fa08
Andris Roling <arol@d-velop.de>
2019-05-10 09:56:49 +0200

    added .gitignore, added material module , added http angular dependency

---
+++ projectpruneangular/src/app/material.module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from "@angular/core";
+
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatGridListModule,
+  MatDividerModule,
+  MatListModule,
+  MatToolbarModule,
+  MatTableModule,
+  MatDialogModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatSortModule
+} from '@angular/material';
+
+@NgModule({
+
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatSortModule,
+  ]
+})
+
+export class MaterialModule {}
